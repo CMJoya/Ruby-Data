72b45cffb4c368fa077bcd20dee60432a4372608
CMJoya <joyacris07@gmail.com>
2016-02-10 22:41:16 -0600

    Create part1.rb

---
+++ part1.rb
@@ -0,0 +1,13 @@
+foods = "Tacos must not have lettuce Only meat and cheese and salsa"
+arrayfoods = foods.split
+
+  puts arrayfoods.inspect
+
+lengtharrays = arrayfoods.map do |lengtharray|
+lengtharray.length
+end
+  
+  puts lengtharrays.inspect
+
+counter = lengtharrays.count(5)
+  puts counter
