1489ad3d39827bb730d905f3913f793f27936870
CMJoya <joyacris07@gmail.com>
2016-02-10 22:42:07 -0600

    Create part2.rb

---
+++ part2.rb
@@ -0,0 +1,42 @@
+movies = []
+
+
+movies << {
+  title: "Forest Gump",
+  budget: 55,
+  stars: ["Tom Hanks"]
+}
+movies << {
+  title: "Star Wars",
+  budget: 11,
+  stars: ["Mark Hamill", "Harrison Ford"]
+}
+movies << {
+  title: "Batman Begins",
+  budget: 150,
+  stars: ["Christian Bale", "Liam Neeson", "Michael Caine"]
+}
+movies << {
+  title: "Titanic",
+  budget: 200,
+  stars: ["Kate Winslet", "Leonardo DiCaprio"]
+}
+movies << {
+  title: "Inception",
+  budget: 160,
+  stars: ["Leonardo DiCaprio", "JGL"]
+}
+
+
+cheap_movies = movies.select do |movie|
+  movie[:budget] < 100
+end
+
+
+puts cheap_movies
+
+leo_movies = movies.select do |movie|
+  movie[:stars].include?("Leonardo DiCaprio")
+end
+
+puts leo_movies
